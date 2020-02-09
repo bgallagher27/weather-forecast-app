67799b36448dd1d140a47bb35aeb146fbf8f0ec9
Brendan Gallagher <bpgallagher27@gmail.com>
2020-02-09 18:29:09 -0500

    push

--- assets/weather.js
+++ assets/weather.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
-
-    let dateToday = moment().format("(MM/DD/YYYY)");
     
-    $(".date").text(dateToday);
+    $(".date").text(moment().format("(MM/DD/YYYY)"));
 
+    let cityInput = "Boston";
+    let searchURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=63ce6f2372091607c7b3b632b1337e15&units=imperial";
+    
 
+    function updateWeather() {
+        $.ajax({
+            url: searchURL,
+            method: "GET",
+        }).then(function (response) {
+            $(".city-name").text(cityInput);
+            $("#today-temp").text(response.main.temp + " degrees F");
+        });
+    };
+
+    $(".search-btn").on("click" , updateWeather());
 
 
 
