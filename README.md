ca04417d4141fd16fc8f30709bc450dd281bc1da
Brendan Gallagher <bpgallagher27@gmail.com>
2020-02-09 21:25:13 -0500

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # weather-forecast-app
+An application to help you plan for any worldwide trip. Enter a city of your choice to receive a current weather report, along with a 5 day forecast for that same city. Your search results will also be saved on the screen for later use.
+
+## Getting Started
+Visit this link to try it out - [https://bgallagher27.github.io/weather-forecast-app/](https://bgallagher27.github.io/weather-forecast-app/)
+
+## Built With
+* [OpenWeather](https://openweathermap.org/) - The weather API
+* [Bootstrap](https://www.getbootstrap.com/) - Bootstrap styles
+
+## User Story
+* AS A traveler
+* I WANT to see the weather outlook for multiple cities
+* SO THAT I can plan a trip accordingly
--- assets/weather.js
+++ assets/weather.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
     
-    $(".date").text(moment().format("(MM/DD/YYYY)"));
+    let today = moment().format("MM/DD/YYYY")
+    $(".date").text(today);
+    
+    let forecastOneDay = moment().add(1, 'day').format("MM/DD/YYYY");
+    let forecastTwoDay = moment().add(2, 'day').format("MM/DD/YYYY");
+    let forecastThreeDay = moment().add(3, 'day').format("MM/DD/YYYY");
+    let forecastFourDay = moment().add(4, 'day').format("MM/DD/YYYY");
+    let forecastFiveDay = moment().add(5, 'day').format("MM/DD/YYYY");
+    
+
+
 
     let cityInput = "Boston";
-    let searchURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=63ce6f2372091607c7b3b632b1337e15&units=imperial";
+    let searchURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=63ce6f2372091607c7b3b632b1337e15&units=imperial&date=" + today;
     
 
     function updateWeather() {
@@ -12,11 +22,90 @@ $(document).ready(function() {
             method: "GET",
         }).then(function (response) {
             $(".city-name").text(cityInput);
-            $("#today-temp").text(response.main.temp + " degrees F");
+            $("#today-temp").text(Math.floor(response.main.temp) + " degrees F");
+            $("#today-humid").text(response.main.humidity + "%");
+            $("#today-wind").text(response.wind.speed + " MPH");
         });
     };
 
-    $(".search-btn").on("click" , updateWeather());
+    let dayTwoURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=63ce6f2372091607c7b3b632b1337e15&units=imperial&date=" + forecastOneDay;
+    
+
+    function dayTwoWeather() {
+        $.ajax({
+            url: dayTwoURL,
+            method: "GET",
+        }).then(function (response1) {
+            $(".date-day-1").text(forecastOneDay);
+            $(".temp-day-1").text(Math.floor(response1.main.temp) + " degrees F");
+            $(".humid-day-1").text(response1.main.humidity + "%");
+        });
+    };
+
+    let dayThreeURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=63ce6f2372091607c7b3b632b1337e15&units=imperial&date=" + forecastTwoDay;
+    
+
+    function dayThreeWeather() {
+        $.ajax({
+            url: dayThreeURL,
+            method: "GET",
+        }).then(function (response2) {
+            $(".date-day-2").text(forecastTwoDay);
+            $(".temp-day-2").text(Math.floor(response2.main.temp) + " degrees F");
+            $(".humid-day-2").text(response2.main.humidity + "%");
+        });
+    };
+
+    let dayFourURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=63ce6f2372091607c7b3b632b1337e15&units=imperial&date=" + forecastThreeDay;
+    
+
+    function dayFourWeather() {
+        $.ajax({
+            url: dayFourURL,
+            method: "GET",
+        }).then(function (response3) {
+            $(".date-day-3").text(forecastThreeDay);
+            $(".temp-day-3").text(Math.floor(response3.main.temp) + " degrees F");
+            $(".humid-day-3").text(response3.main.humidity + "%");
+        });
+    };
+
+    let dayFiveURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=63ce6f2372091607c7b3b632b1337e15&units=imperial&date=" + forecastFourDay;
+    
+
+    function dayFiveWeather() {
+        $.ajax({
+            url: dayFiveURL,
+            method: "GET",
+        }).then(function (response4) {
+            $(".date-day-4").text(forecastFourDay);
+            $(".temp-day-4").text(Math.floor(response4.main.temp) + " degrees F");
+            $(".humid-day-4").text(response4.main.humidity + "%");
+        });
+    };
+
+    let daySixURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=63ce6f2372091607c7b3b632b1337e15&units=imperial&date=" + forecastFiveDay;
+    
+
+    function daySixWeather() {
+        $.ajax({
+            url: daySixURL,
+            method: "GET",
+        }).then(function (response5) {
+            $(".date-day-5").text(forecastFiveDay);
+            $(".temp-day-5").text(Math.floor(response5.main.temp) + " degrees F");
+            $(".humid-day-5").text(response5.main.humidity + "%");
+        });
+    };
+
+    $(".search-btn").on("click" , 
+        updateWeather(), 
+        dayTwoWeather(), 
+        dayThreeWeather(), 
+        dayFourWeather(),
+        dayFiveWeather(),
+        daySixWeather(),
+    );
 
 
 
